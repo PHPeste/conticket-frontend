9eda70507b3f080319cd22199ec1be47850e9d82
Franklin Rabay <franklinrabay@MacBook-Pro-de-Franklin.local>
2015-08-26 08:45:44 -0300

    Commit Inicial

---
+++ coffee/controllers/layout/default_ctrl.coffee
@@ -0,0 +1,6 @@
+angular.module "ConticketApp", []
+.controller 'DefaultCtrl', [
+  '$scope'
+  ($scope)->
+    $scope.nome = "Denis Peteca"
+]
---
+++ scripts/scripts.js
@@ -0,0 +1,3 @@
+(function(){angular.module("ConticketApp",["ngMaterial"]).config(function(t){return t.theme("default").primaryPalette("cyan").accentPalette("light-blue")})}).call(this);
+(function(){}).call(this);
+(function(){angular.module("ConticketApp",[]).controller("DefaultCtrl",["$scope",function(e){return e.nome="Denis Peteca"}])}).call(this);
